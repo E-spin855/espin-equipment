3e3f67ca71e70d261c9bb8b083877144e148b09b
E-spin855 <info@espinmedical.com>
2026-03-12 17:08:22 -0700

    upadte

--- api/projects.js
+++ api/projects.js
@@ -1,10 +1,11 @@
 import { Pool } from "pg";
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+console.log("ENV DATABASE_URL:", process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-
 /* ===============================
    CORS
 =============================== */
