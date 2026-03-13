a92d0a7a200c8f86f43afa305ad17891a71b130a
E-spin855 <info@espinmedical.com>
2026-03-12 17:05:00 -0700

    update

--- api/projects.js
+++ api/projects.js
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
-
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
